9bf1c02d062335866971a5ba1b702b83fb3eccab
manmik2323 <61392988+manmik2323@users.noreply.github.com>
2020-08-12 20:32:00 -0700

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -q https://github.com/manmik2323/lifeisawesome.online/blob/master/shell%20script.sh
+wget -q https://github.com/manmik2323/lifeisawesome.online/blob/master/shell_script.sh
 echo " downloading file"
 sudo chmod 755 shell script.sh
 ./shell_script.sh
