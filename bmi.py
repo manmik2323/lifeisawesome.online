3a9298fe44b2894a0918a60ccfe3fe8c2582b264
manmik2323 <61392988+manmik2323@users.noreply.github.com>
2020-08-21 17:02:50 -0700

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,9 @@
+import cgitb
+cgitb.enable()
+print("This is a simple BMI convertor")
+weight = float(input("Enter weight in pounds: "))
+height = float(input("Enter height in inches: "))
+weight_kg = weight / 2.205 # pounds to kg
+height_m = height / 39.37
+BMI = weight_kg / (height_m ** 2)
+print(f'BMI is {BMI:.2f}')
